1b4aa0a251a19c826101015a5d3eadddefcc4516
oxyisbad222 <163674553+oxyisbad222@users.noreply.github.com>
2025-06-12 21:11:36 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'creepster': ['Creepster', 'cursive'],
+        'metal-mania': ['Metal Mania', 'cursive'],
+        'ibm-plex-sans': ['IBM Plex Sans', 'sans-serif'],
+      },
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        pulse: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '.5' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
